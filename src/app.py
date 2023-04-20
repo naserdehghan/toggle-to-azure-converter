5c9020693ce57eb29bfdcbf9055e9727b5ea8eb2
naser dehghan <naserdehghan@outlook.com>
2023-04-20 17:08:22 +0330

    🔧 chore(.env.example): add state variable to example file 🚀 feat(app.py): add support for state variable in .env file to set State column in result.csv file

--- src/app.py
+++ src/app.py
@@ -2,16 +2,17 @@ from dotenv import load_dotenv
 import pandas as pd
 import os
 
+# Load env variables
 load_dotenv()
+state = os.getenv('state')
 assigned_to = os.getenv('assigned_to')
 iteration_path = os.getenv('iteration_path')
 
 # Read the original CSV file
 original_data = pd.read_csv('files/source.csv')
 
-# Rename the columns
-new_columns = {'Description': 'Title'}
-new_data = original_data.rename(columns=new_columns)\
+# Rename the columns and drop some others
+new_data = original_data.rename(columns={'Description': 'Title'})\
     .drop(columns=['Client', 'Project', 'Duration'])
 
 # Split the Duration column into separate columns for hours, minutes, and seconds
@@ -21,13 +22,20 @@ duration_split = original_data['Duration']\
     .astype(int)
 
 # Calculate the total duration in hours as a float
-new_data['Remaining Work'] = 0
 new_data['Completed Work'] = (duration_split[0] + duration_split[1]/60 + duration_split[2]/3600)\
     .round(decimals=3)
 new_data['Original Estimate'] = new_data['Completed Work']
-new_data['State'] = "New"
+
+# Fill static variables
+new_data['Remaining Work'] = 0
 new_data['Work Item Type'] = "Task"
 
+if not state == None:
+    new_data['State'] = state
+else:
+    new_data['State'] = "New"
+
+# Fill personal variables
 if not assigned_to == None:
     new_data['Assigned To'] = assigned_to
 
@@ -35,5 +43,5 @@ if not iteration_path == None:
     new_data['Iteration Path'] = iteration_path
 
 
-# Write the renamed data to a new CSV file
+# Write the result to a new CSV file
 new_data.to_csv('files/result.csv', index=False)
